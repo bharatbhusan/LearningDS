edde6da8cc3a67526d584dc33d6476a57c3adcbc
Bharat <bharatbhusanmantry@gmail.com>
2021-06-27 16:38:20 +0530

    commit stack

---
+++ src/stack/BalancedParanthesis.java
@@ -0,0 +1,37 @@
+package stack;
+
+import java.util.Stack;
+
+public class BalancedParanthesis {
+
+	public static void main(String[] args) 
+	
+	{
+		
+		String str = "{}[] {([])]}";
+		System.out.println(isValidParanthesis(str));
+	}
+	
+	public static boolean isValidParanthesis(String str)
+	{
+		
+		if(str.length()%2!=0 || str.length()==0)
+			return false;
+		
+		Stack<Character> st = new Stack<Character>();
+		for(char ch:str.toCharArray())
+		{
+			if(ch=='{') st.push('}');
+			else if(ch=='(') st.push(')');
+			else if(ch=='[') st.push(']');
+			else 
+				{
+				if(st.isEmpty() || st.pop()!=ch) return false;
+				}
+					
+		}
+		if(!st.isEmpty()) return false;
+		
+		return true;
+	}
+}
